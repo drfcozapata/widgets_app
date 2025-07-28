8b4085706387d6f69642c6b5918d67d3cd304af0
Francisco Zapata <drfcozapata@gmail.com>
2025-07-28 18:08:15 -0400

    Configured go router

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -18,16 +19,6 @@ class _HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // List<appMenuItems> appMenuItems = [];
-
-    // return ListView.builder(
-    //   itemCount: appMenuItems.length,
-    //   prototypeItem: ListTile(title: Text(appMenuItems.first)),
-    //   itemBuilder: (context, index) {
-    //     return ListTile(title: Text(appMenuItems[index]));
-    //   },
-    // );
-
     return ListView.builder(
       itemCount: appMenuItems.length,
       itemBuilder: (context, index) {
@@ -54,10 +45,7 @@ class _CustomListTile extends StatelessWidget {
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subtitle),
       onTap: () {
-        // Navigator.of(
-        //   context,
-        // ).push(MaterialPageRoute(builder: (context) => const ButtonsScreen()));
-        Navigator.pushNamed(context, menuItem.link);
+        context.push(menuItem.link);
       },
     );
   }
